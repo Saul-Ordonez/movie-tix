040906c4d668e93bda0b08aa49c35ae988be3cbf
Student <example@example.com>
2019-11-05 12:02:24 -0800

    add user interface and business logic

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+function Ticket(movieName, movieTime, userAge, ticketPrice) {
+  this.movieName = movieName,
+  this.movieTime = movieTime,
+  this.userAge = userAge,
+  this.ticketPrice = 10
+}
+
+// function generateTicket() {
+  Ticket.prototype.changeValue = function() {
+    if (this.movieName === 'spiderman') {
+      return this.ticketPrice += 2;
+    }
+    if (this.movieTime === 'matinee') {
+      return this.ticketPrice -= 2;
+    }
+    if (this.userAge === 'young' || this.userAge === 'senior') {
+      return this.ticketPrice -= 2;
+    }
+  }
+
+  change last if statement
+  //   return this.ticketPrice;
+  //   console.log(this.ticketPrice);
+  // }
+  //
+  // Ticket.prototype.minusValue = function() {
+  //   this.ticketPrice -= 2;
+  //   return this.ticketPrice.minusValue;
+  // }
+  //
+  //
+    // if (this.movieTime === 'matinee') {
+    //   return this.ticketPrice.minusValue();
+    // }
+  //   if (this.userAge === 'young' || this.userAge === 'senior') {
+  //     return this.ticketPrice.minusValue();
+  //   }
+  // // }
+
+
+$(document).ready(function() {
+  $("form#movie-input").submit(function(event) {
+    event.preventDefault();
+    var movie = $('input[name=movie-name]:checked').val();
+    var time = $('input[name=movie-time]:checked').val();
+    var age = $('input[name=age]:checked').val();
+
+    var newTicket = new Ticket (movie, time, age);
+    newTicket.changeValue();
+    console.log(newTicket);
+  });
+});
